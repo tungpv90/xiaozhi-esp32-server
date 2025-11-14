5f7ee5041c329237d74ff36556a82db71533fcae
tungpv90 <tungpv90@yahoo.com>
2025-11-14 15:59:27 +0700

    commit change logo
    -change logo
    change docker-composer-all.yml to own account

--- main/manager-web/src/views/login.vue
+++ main/manager-web/src/views/login.vue
@@ -9,7 +9,6 @@
             margin-left: 10px;
             gap: 10px;
           ">
-          <img loading="lazy" alt="" src="@/assets/xiaozhi-logo.png" style="width: 45px; height: 45px" />
           <img loading="lazy" alt="" src="@/assets/xiaozhi-ai.png" style="height: 18px" />
         </div>
       </el-header>
